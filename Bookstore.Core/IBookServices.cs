609507527f705330d69b8c9fbad12a966aa902e5
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-02-03 04:52:38 +0530

    Add project files.

---
+++ Bookstore.Core/IBookServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookstore.Core
+{
+    public interface IBookServices
+    {
+        List<Book> GetBooks();
+    }
+}
---
+++ Bookstore.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {        
+        public BooksController()
+        {
+        }        
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+
+        }
+    }
+}
